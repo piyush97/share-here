a7cdd648015405f7e48fe3586ec986037de02352
Piyush Mehta <me@piyushmehta.com>
2020-09-21 23:55:34 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
+  orm.getMigrator().up();
   const post = orm.em.create(Post, { title: "my first post" });
-
   await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 console.log("hello world");
 
