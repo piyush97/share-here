1815c83c80724b280b7bd6721bcff311af3afc1d
Piyush Mehta <me@piyushmehta.com>
2020-09-21 23:36:47 +0530

    fixes and configurations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { Post } from "./entities/post";
+import microConfig from "./micro-orm.config";
+
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: "share-here",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+
+  await orm.em.persistAndFlush(post);
 };
 console.log("hello world");
+
+main().catch((err) => {
+  console.log(err);
+});
